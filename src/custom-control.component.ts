1c5c4dec3d8766133b4d519b00edb8f9dd45c7eb
Peter Tandler <peter.tandler@etalytics.com>
2025-12-23 11:53:09 +0100

    try NgxControlValueAccessor

--- src/custom-control.component.ts
+++ src/custom-control.component.ts
@@ -1,21 +1,14 @@
-import { JsonPipe } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   effect,
-  input,
+  inject,
   model,
+  untracked,
 } from '@angular/core';
-import {
-  DisabledReason,
-  Field,
-  form,
-  FormValueControl,
-  max,
-  required,
-  ValidationError,
-  WithOptionalField,
-} from '@angular/forms/signals';
+import { Field, form, FormValueControl } from '@angular/forms/signals';
+import { isEqual } from 'lodash-es';
+import { NgxControlValueAccessor } from 'ngxtension/control-value-accessor';
 
 export interface Test {
   stringValue: string;
@@ -24,14 +17,39 @@ export interface Test {
 
 @Component({
   selector: 'custom-form-value-control',
-  template: `<div style="border: gray solid 2px">
-    <input type="text" [field]="valueForm.stringValue"/>
-    <input type="number" [field]="valueForm.numberValue"/>
-   </div>`,
+  template: ` <div style="border: gray solid 2px">
+    <input type="text" [field]="valueForm.stringValue" />
+    <input type="number" [field]="valueForm.numberValue" />
+  </div>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [Field],
+  hostDirectives: [NgxControlValueAccessor],
 })
 export class CustomControlComponent implements FormValueControl<Test> {
-  public readonly value = model<Test>({ stringValue: 'x', numberValue: 42 });
-  protected readonly valueForm = form(this.value);
+  protected readonly cva = inject(NgxControlValueAccessor<Test>);
+  public readonly value = model<Test>({
+    stringValue: 'ho! ho! ho!',
+    numberValue: 24,
+  });
+  protected readonly valueForm = form(this.value, {
+    name: 'CustomControlComponent Form',
+  });
+  constructor() {
+    // sync model to CVA
+    effect(() => {
+      const modelValue = this.value();
+      const cvaValue = untracked(this.cva.value$);
+      if (!isEqual(modelValue, cvaValue)) {
+        this.cva.value$.set(modelValue);
+      }
+    });
+    // sync CVA to model
+    effect(() => {
+      const modelValue = untracked(this.value);
+      const cvaValue = this.cva.value$();
+      if (!isEqual(modelValue, cvaValue)) {
+        this.value.set(cvaValue);
+      }
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,33 @@
+import { JsonPipe } from '@angular/common';
 import {
   Component,
   provideZonelessChangeDetection,
   signal,
 } from '@angular/core';
-import { JsonPipe } from '@angular/common';
-import { Field, form, maxLength } from '@angular/forms/signals';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { form } from '@angular/forms/signals';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { CustomControlComponent, Test } from './custom-control.component';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
   template: `
     <h4>Field {{ value() | json }}</h4>
-    <custom-form-value-control [field]="valueForm" /><br/>
+    <!--    <custom-form-value-control [field]="valueForm" /><br />-->
     <h4>FormControl {{ formControlValue() | json }}</h4>
-    <form>
     <custom-form-value-control [formControl]="formControl" />
-    </form><br/>
+    <br />
   `,
-  imports: [CustomControlComponent, Field, ReactiveFormsModule, JsonPipe],
+  imports: [ReactiveFormsModule, JsonPipe, CustomControlComponent],
 })
 export class App {
-  public readonly value = signal<Test>({ stringValue: 'hi!', numberValue: 17 });
-  public readonly valueForm = form(this.value);
+  public readonly value = signal<Test>({ stringValue: 'hi!', numberValue: 42 });
+  public readonly valueForm = form(this.value, { name: 'App Form' });
 
   // reactive form interop
   public readonly formControl = new FormControl();
   protected readonly formControlValue = toSignal(this.formControl.valueChanges);
 }
+// noinspection JSIgnoredPromiseFromCall
 bootstrapApplication(App, { providers: [provideZonelessChangeDetection()] });
